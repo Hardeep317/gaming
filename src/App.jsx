4c9df693870ae4ba66f7ac3eb58a77d80e931043
Hardeep <hardeep41016@gmail.com>
2024-12-22 11:57:23 +0530

    adding code to repo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Hero from './components/Hero'
+import About from './components/About'
+import Navbar from './components/navbar'
+import Features from './components/Features'
+
+const App = () => {
+  return (
+    <div className="relative min-h-screen w-screen overflow-x-hidden bg-zinc-600">
+      <Navbar />
+      <Hero/>
+    <About />
+    <Features />
+    </div>
+  )
+}
+
+export default App
